7ed6f5a6d35f394fa7943d699d0e690acdecc517
FranSal <rubycscf@me.com>
2015-03-27 16:18:07 +0100

    conversion to  sqrt2(x) function

--- mysqrt.py
+++ mysqrt.py
@@ -3,15 +3,16 @@
 sqrt using the Newton mwthod
 
 """
-x=10000
-s=1
-kmax=100
-delta=1e-14
-for k in range(kmax):
-        print "Before iteration %s, s = %s" %(k,s)
-        s0=s
-        s= 0.5 *(s+x/s)
-        if abs(s-s0)/x < delta:
-            break
-print ("\nAfter %s iterations, s = %s" %(k+1,s))
+
+def sqrt2(x):
+    s=1
+    kmax=100
+    delta=1e-14
+    for k in range(kmax):
+            print "Before iteration %s, s = %20.15f" %(k,s)
+            s0=s
+            s= 0.5 *(s+x/s)
+            if abs(s-s0)/x < delta:
+                break
+    print ("\nAfter %s iterations, s = %20.15f" %(k+1,s))
 
