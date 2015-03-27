c21a1b8b19a2bf910e3640600d55653cd3188ac7
FranSal <rubycscf@me.com>
2015-03-27 16:10:44 +0100

    added convergence test

--- mysqrt.py
+++ mysqrt.py
@@ -3,9 +3,15 @@
 sqrt using the Newton mwthod
 
 """
+x=10000
 s=1
-x=9
-for k in range(6):
+kmax=100
+delta=1e-14
+for k in range(kmax):
+        print "Before iteration %s, s = %s" %(k,s)
+        s0=s
         s= 0.5 *(s+x/s)
-        
+        if abs(s-s0)/x < delta:
+            break
+print ("\nAfter %s iterations, s = %s" %(k+1,s))
 
